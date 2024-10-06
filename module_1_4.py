61564bd95335623e6e30dbe8d0a7e0353f574af5
Aleksander005 <dmitrijdavydov853@gmail.com>
2024-10-06 16:09:12 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,9 @@
+
+my_string = input('Привет!')
+count = len('llllllllllо') # не понимаю почему не работает функция
+print (my_string.upper())
+print (my_string.lower())
+print (my_string.replace(__old:('Привет!'), __new:('Gjrf')) # не понимаю почему не работает функция
+print (my_string[0:1])
+print (my_string[-1])
+
